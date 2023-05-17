c7454282cd70bb87fc1e7250a1dc6f93c81ddaac
Corey Gulledge <coreygulledge35@gmail.com>
2023-05-17 16:10:42 -0400

    final script

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var specialCharacters = "!@#$%-?";
+  var numbers = "0123456789";
+  var characterBase = "";
+  var password = "";
+  var length = prompt("choose length between 8-128 characters");
+  if (length < 8|| length > 128) {
+    alert("Must choose between 8-128 characters!");
+    return generatePassword();
+  }
+  if (confirm("would you like to have lowercase letters?")) {
+    characterBase += lowercaseLetters;
+  }
+  if (confirm("Would you to have uppercase letters?")) {
+    characterBase += uppercaseLetters;
+  }
+  if (confirm("Would you like to have special characters?")) {
+    characterBase += specialCharacters;
+  }
+  if (confirm("Would you like to have numbers?")) {
+    characterBase += numbers;
+  }
+  if (characterBase.length < 1) {
+    alert("Must choose at least one character type");
+    return generatePassword();
+  }
+  for (let i = 0; i < length; i++) {
+    password += characterBase.charAt(Math.floor(Math.random()*characterBase.length));
+   
+  }
+  return password;
+}
